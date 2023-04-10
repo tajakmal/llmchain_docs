f624f93a03eb2dad74744db6cdc0efe19b65ecf3
tajakmal <tajwar.akmal@gmail.com>
2023-04-10 18:53:41 -0400

    updated code to purge old tokens

--- main.py
+++ main.py
@@ -3,13 +3,36 @@ import openai
 import os
 from dotenv import load_dotenv
 from IPython.display import Markdown, display
+import tiktoken
 
 def display_answer(answer):
     display(Markdown(answer))
 
+tokenizer = tiktoken.get_encoding('p50k_base')
+def count_tokens(text):
+    tokens = tokenizer.encode(
+        text,
+        disallowed_special=()
+    )
+    return len(tokens)
+
+def truncate_conversation(conversation, max_tokens=4096):
+    total_tokens = 0
+    truncated_conversation = []
+
+    for message in reversed(conversation):
+        message_tokens = count_tokens(message['content'])
+        total_tokens += message_tokens
+
+        if total_tokens < max_tokens:
+            truncated_conversation.insert(0, message)
+        else:
+            break
+
+    return truncated_conversation
+
 def get_gpt4_answer(messages):
     response = openai.ChatCompletion.create(
-        #model="gpt-4",
         model="gpt-3.5-turbo",
         messages=messages
     )
@@ -26,27 +49,20 @@ def get_augmented_query(query, index, embed_model):
     augmented_query = "\n\n---\n\n".join(contexts)+"\n\n-----\n\n"+query
     return augmented_query
 
-# Load environment variables
 load_dotenv()
 
-# Initialize OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 embed_model = "text-embedding-ada-002"
 
-# Initialize Pinecone connection
 pinecone.init(
     api_key=os.getenv("PINECONE_API_KEY"),
     environment="us-central1-gcp"
 )
 
-# Connect to Pinecone index
 index_name = 'gpt-4-langchain-docs'
 index = pinecone.Index(index_name)
 
-# System message to prime the GPT-4 model
-#primer = f"""You are Q&A bot. A highly intelligent system that answers user questions based on the information provided by the user above each question. If the information can not be found in the information provided by the user you truthfully say "I don't know"."""
 primer = f"""You are Q&A bot. A highly intelligent system that answers user questions. You will first try to answer based on the information provided by the user above each question. If the information cannot be found in the information provided by the user then use your own pre-trained model."""
-# Initialize conversation with the system message
 conversation = [{"role": "system", "content": primer}]
 
 print("You can now chat with the AI. Type 'exit' to end the conversation.")
@@ -56,20 +72,14 @@ while True:
     if user_input.lower() == 'exit':
         break
 
-    # Add user input to the conversation
     conversation.append({"role": "user", "content": user_input})
 
-    # Get the augmented query
     augmented_query = get_augmented_query(user_input, index, embed_model)
-
-    # Add augmented query to the conversation
     conversation.append({"role": "user", "content": augmented_query})
 
-    # Get GPT-4's response
-    gpt4_response = get_gpt4_answer(conversation)
+    truncated_conversation = truncate_conversation(conversation)
+    gpt4_response = get_gpt4_answer(truncated_conversation)
 
-    # Add GPT-4's response to the conversation
     conversation.append({"role": "assistant", "content": gpt4_response})
 
-    # Display the response
     print("AI:", gpt4_response)
